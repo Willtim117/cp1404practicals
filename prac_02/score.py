15160ec0cf7655db47af27626ed1e4558b283623
Willtim117 <william.tim@my.jcu.edu.au>
2026-09-25 02:02:10 +1000

    refactors score program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,30 +3,6 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-while True:
-    user_input = input("Enter score (or Q to quit): ").strip()
-
-    if user_input.upper() == "Q":
-        print("Thank you")
-        break
-
-    try:
-        score = float(user_input)
-    except ValueError:
-        print("Invalid Input")
-        continue
-
-    if score < 0 or score > 100:
-        print("Invalid score")
-    elif score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Passable")
-    elif score >= 0:
-        print("Bad")
-    else:
-        print("Invalid Input")
-
 
 def main():
     while True:
@@ -42,11 +18,8 @@ def main():
             print("Invalid Input")
             continue
 
-        else:
-            result = categorize_result(user_input)
-            print(result)
-
-
+        result = categorize_result(score)
+        print(result)
 
 
 def categorize_result(score: float) -> str:
@@ -62,3 +35,5 @@ def categorize_result(score: float) -> str:
         result = "Invalid Input"
     return result
 
+
+main()
