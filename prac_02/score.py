ead896ee88905263bb9d44f34519afe08c256b09
Willtim117 <william.tim@my.jcu.edu.au>
2026-09-25 01:28:02 +1000

    keeps original code has added a copy of the code introducing functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,62 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+while True:
+    user_input = input("Enter score (or Q to quit): ").strip()
+
+    if user_input.upper() == "Q":
+        print("Thank you")
+        break
+
+    try:
+        score = float(user_input)
+    except ValueError:
+        print("Invalid Input")
+        continue
+
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    elif score >= 0:
+        print("Bad")
+    else:
+        print("Invalid Input")
+
+
+def main():
+    while True:
+        user_input = input("Enter score (or Q to quit): ").strip()
+
+        if user_input.upper() == "Q":
+            print("Thank you")
+            break
+
+        try:
+            score = float(user_input)
+        except ValueError:
+            print("Invalid Input")
+            continue
+
+        else:
+            result = categorize_result(user_input)
+            print(result)
+
+
+
+
+def categorize_result(score: float) -> str:
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score >= 90:
+        result = "Excellent"
+    elif score >= 50:
+        result = "Passable"
+    elif score >= 0:
+        result = "Bad"
+    else:
+        result = "Invalid Input"
+    return result
 
